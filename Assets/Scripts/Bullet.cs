46afa6354c184297c3d9cde6aca4eccdbdc5e1a0
fuccpuff <fuccpuff@gmail.com>
2024-01-23 15:57:08 +0500

    add script to shooting

---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speedBullet = 10f;
+    GameManager gameManager;
+
+    private void Start()
+    {
+        gameManager = GameObject.FindObjectOfType<GameManager>();
+        Rigidbody2D rigidBody = GetComponent<Rigidbody2D>();
+        rigidBody.velocity = new Vector2(0, speedBullet);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(collision.gameObject);
+        gameManager.AddScore();
+        Destroy(gameObject);
+    }
+}
